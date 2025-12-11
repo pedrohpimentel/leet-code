49ef204db567517d2108394c64c2639f78903e34
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2025-12-11 11:57:24 -0300

    Conversor de temperatura

--- src/main/java/Semana1/Ex2.java
+++ src/main/java/Semana1/Ex2.java
@@ -3,13 +3,21 @@ package Semana1;
 import java.util.Locale;
 import java.util.Scanner;
 
-/**/
+/*
+Conversor de TemperaturaCrie um programa que:
+Declare uma variável celsius e atribua a ela uma temperatura em graus Celsius (ex: 25).
+Converta essa temperatura para Fahrenheit usando a fórmula: F = C * (9/5) + 32.
+(Atenção com a divisão de inteiros em Java!) Imprima o resultado em Fahrenheit.
+*/
 public class Ex2 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int celsius = 25;
+        double farenheit = (double) celsius * (9.0/5.0) + 32;
 
+        System.out.println("Farenheit de " + celsius + " é = " + farenheit );
 
         sc.close();
     }
