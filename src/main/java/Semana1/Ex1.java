a780edf82728293a4f1079e21875eb83a010aa0c
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2025-12-11 11:40:17 -0300

    Cálculo Simples

--- src/main/java/Semana1/Ex1.java
+++ src/main/java/Semana1/Ex1.java
@@ -1,33 +1,28 @@
 package Semana1;
 
-
 import java.util.Locale;
 import java.util.Scanner;
 
 /*
- Algoritmo Sequencial e Condicional (Cálculo Simples) Problema:
- Crie um programa em Java que peça ao usuário para inserir a base e a altura de um retângulo.
- O programa deve calcular e imprimir a área do retângulo. Além disso, se a área for maior que $100$,
- imprima a mensagem "Retângulo Grande".
- */
+Cálculo Simples
+Crie um programa que:
+
+Declare duas variáveis, numero1 e numero2, e atribua valores inteiros a elas (ex: 15 e 7).
+
+Calcule e armazene em uma nova variável a soma desses dois números.
+
+Imprima o resultado da soma.
+*/
 public class Ex1 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insira o valor da base e da altura de um retângulo!");
-        System.out.print("Valor base: ");
-        double valorBase = sc.nextInt();
+    int numero1 = 15;
+    int numero2 = 7;
 
-        System.out.print("Valor altura: ");
-        double valorAltura = sc.nextInt();
-
-        double areaRetangulo = valorBase * valorAltura;
-
-        System.out.println("O valor do retângulo é : " + areaRetangulo);
-        if (areaRetangulo > 100){
-            System.out.println("Retângulo grande.");
-        }
+    int soma = numero1 + numero2;
+        System.out.println("Soma de " + numero1 + " + " + numero2 + " é = " + soma);
 
         sc.close();
     }
--- src/main/java/Semana1/Ex5.java
+++ src/main/java/Semana1/Ex5.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 /**/
-public class Ex2 {
+public class Ex5 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
